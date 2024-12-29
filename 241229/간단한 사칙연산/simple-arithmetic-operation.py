a87a9bf83b13926156f911484e46c61becb10bdf
seoli <139112248+sh694090@users.noreply.github.com>
2024-12-29 17:26:50 +0900

    Title: 간단한 사칙연산, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241229/간단한 사칙연산/simple-arithmetic-operation.py
@@ -0,0 +1,17 @@
+# 두 정수 a, b를 한 줄로 입력한다
+n = input().split()
+
+a = int(n[0])
+b = int(n[1])
+
+# a + b의 결과를 출력한다
+print(f"{a + b}")
+
+# a - b의 결과를 출력한다
+print(f"{a - b}")
+
+# a // b의 결과를 출력한다
+print(f"{a // b}")
+
+# a % b의 결과를 출력한다
+print(f"{a % b}")
