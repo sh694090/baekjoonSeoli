710e6270a9ba95bbd8067f6665920d73f1791812
seoli <139112248+sh694090@users.noreply.github.com>
2024-12-29 12:22:32 +0900

    Title: 정수 세 개 입력받아 출력, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241229/정수 세 개 입력받아 출력/take-three-integers-and-output.py
@@ -0,0 +1,13 @@
+# 첫 번째 줄에 정수 a, b을 공백을 사이에 두고 입력한다
+arr = input().split()
+
+a = int(arr[0])
+b = int(arr[1])
+
+# 두 번째 줄에 정수 c를 입력한다
+c = input()
+
+c = int(c)
+
+# 정수 a, b, c를 사이에 공백을 두고 순서대로 출력한다
+print(a, b, c)
