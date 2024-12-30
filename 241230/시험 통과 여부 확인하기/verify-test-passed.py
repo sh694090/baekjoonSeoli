e39794485858cac7fa367b198c0c6fcc6686eec0
seoli <139112248+sh694090@users.noreply.github.com>
2024-12-30 11:40:58 +0900

    Title: 시험 통과 여부 확인하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241230/시험 통과 여부 확인하기/verify-test-passed.py
@@ -0,0 +1,9 @@
+# 정수 n을 입력한다
+n = int(input())
+
+# 정수 n >= 80인 경우 pass 출력
+if n >= 80:
+    print('pass')
+# 정수 n < 80인 경우 "(80 - n) more score"의 결과 출력
+if n < 80:
+    print("(80 - n) more score")
