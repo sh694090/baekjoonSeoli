84e11fa7a7853b74e41ef574e6065e7303d6bf23
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-01 14:07:40 +0900

    Title: 알파벳에 따른 평가, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250101/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,26 @@
+# 영문 대문자를 입력한다
+string = input()
+
+# S인 경우
+if string == 'S':
+    print("Superior")
+
+# A인 경우
+elif string == 'A':
+    print("Excellent")
+
+# B인 경우
+elif string == 'B':
+    print("Good")
+
+# C인 경우
+elif string == 'C':
+    print("Usually")
+
+# D인 경우
+elif string == 'D':
+    print("Effort")
+
+# 그 이외의 경우
+else:
+    print("Failure")
