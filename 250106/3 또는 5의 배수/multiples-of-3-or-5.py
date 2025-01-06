97b7d161a6516dcfe0e232e43ff318b52581ae38
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-06 10:55:27 +0900

    Title: 3 또는 5의 배수, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250106/3 또는 5의 배수/multiples-of-3-or-5.py
@@ -0,0 +1,14 @@
+# 정수 a를 입력한다
+a = int(input())
+
+# 정수 a가 3의 배수인 경우 'YES', 아닌 경우 'NO' 출력
+if a % 3 == 0:
+    print('YES')
+else:
+    print('NO')
+
+# 정수 a가 5의 배수인 경우 'YES', 아닌 경우 'NO' 출력
+if a % 5 == 0:
+    print('YES')
+else:
+    print('NO')
