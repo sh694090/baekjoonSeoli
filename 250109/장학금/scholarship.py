90a3174e3afcdf819f34885f0d8ed4b590aed9c1
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-09 18:24:30 +0900

    Title: 장학금, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250109/장학금/scholarship.py
@@ -0,0 +1,17 @@
+# 중간, 기말 점수를 공백에 사이에 두고 입력한다
+mid, final = input().split()
+
+mid = int(mid)
+final = int(final)
+
+# 중간 >= 90 and 기말 >= 95이면 100000
+if mid >= 90 and final >= 95:
+    print(100000)
+    
+# 중간 >= 90 and 기말 > 90이면 50000
+elif mid >= 90 and final > 90:
+    print(50000)
+
+# 이외는 0
+else:
+    print(0)
