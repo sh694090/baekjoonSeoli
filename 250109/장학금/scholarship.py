fba720f7335250f9bbd56929613b0f410e6176da
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-09 18:26:12 +0900

    Title: 장학금, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250109/장학금/scholarship.py
+++ 250109/장학금/scholarship.py
@@ -7,9 +7,9 @@ final = int(final)
 # 중간 >= 90 and 기말 >= 95이면 100000
 if mid >= 90 and final >= 95:
     print(100000)
-    
+
 # 중간 >= 90 and 기말 > 90이면 50000
-elif mid >= 90 and final > 90:
+elif mid >= 90 and final >= 90:
     print(50000)
 
 # 이외는 0
