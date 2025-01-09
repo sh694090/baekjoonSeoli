1f69dcb7e77b7e3d4e42611f3b1f913daf904037
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-09 12:46:39 +0900

    Title: 4번의 크기 비교, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250109/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,34 @@
+# 정수 a가 주어진다
+a = int(input())
+
+# 정수 b, c, d, e가 공백을 두고 주어진다
+b, c, d, e = input().split()
+
+b = int(b)
+c = int(c)
+d = int(d)
+e = int(e)
+
+# a > b
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# a > c
+if a > c:
+    print(1)
+else:
+    print(0)
+
+# a > d
+if a > d:
+    print(1)
+else:
+    print(0)
+
+# a > e
+if a > e:
+    print(1)
+else:
+    print(0)
