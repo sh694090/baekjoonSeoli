2676a72b0afb05bc345b2a18082a5ec5bc0ac64b
seoli <139112248+sh694090@users.noreply.github.com>
2025-01-09 12:22:20 +0900

    Title: 4가지 관계연산자, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250109/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,29 @@
+# 정수 a, b를 공백을 사이에 두고 입력한다
+a, b = input().split()
+
+a = int(a)
+b = int(b)
+
+# a >= b
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# a > b
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# b >= a
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+# b > a
+if b > a:
+    print(1)
+else:
+    print(0)
