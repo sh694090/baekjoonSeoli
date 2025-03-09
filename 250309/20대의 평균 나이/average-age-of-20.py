c96e49a2a0216d0ba90f4d69c34bc7677df34ce1
seoli <139112248+sh694090@users.noreply.github.com>
2025-03-09 13:40:09 +0900

    Title: 20대의 평균 나이, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/20대의 평균 나이/average-age-of-20.py
+++ 250309/20대의 평균 나이/average-age-of-20.py
@@ -1,14 +1,20 @@
 from sys import stdin as s
 
-cnt = 0
 total = 0
+cnt = 0
+
 while True:
+# 나이를 입력받는다
     age = int(s.readline().rstrip())
 
-    if age >= 30:
-        print(f"{avg:.2f}")
+# 입력받는 나이가 20대인지 확인 후 20대가 아니면 break
+    if age < 20 or age >= 30:
         break
 
-    cnt += 1
+# 20대이면 total에 더함
     total += age
+    cnt += 1
+
     avg = total / cnt
+
+print(f"{avg:.2f}")
