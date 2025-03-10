bb4998b72e45e8d5602ba2c915f9dbc67e1726f5
seoli <139112248+sh694090@users.noreply.github.com>
2025-03-10 20:25:04 +0900

    Title: 별표 출력하기 5, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250310/별표 출력하기 5/print-star-5.py
@@ -0,0 +1,10 @@
+from sys import stdin as s
+
+N = int(s.readline().rstrip())
+
+for i in range(N):
+    for j in range(N - i):
+        for k in range(N - i):
+            print("*", end = "")
+        print("", end = " ")
+    print()
