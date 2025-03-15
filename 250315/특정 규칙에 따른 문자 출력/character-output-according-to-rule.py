712c79052fa1359eea2389cf3ce56481ce1d3d0f
seoli <139112248+sh694090@users.noreply.github.com>
2025-03-15 22:06:37 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250315/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -0,0 +1,17 @@
+from sys import stdin as s
+
+N = int(s.readline().rstrip())
+
+# 공백 먼저 출력하고 @ 출력
+
+for i in range(N):
+    for j in range(1, N - i):
+        print(" ", end = " ")
+    for j in range(i + 1):
+        print("@", end = " ")
+    print()
+
+for i in range(1, N):
+    for j in range(N - i):
+        print("@", end = " ")
+    print()
