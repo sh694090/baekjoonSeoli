44a7dd4a045d6aba40810aa9eaedcd8e76978032
seoli <139112248+sh694090@users.noreply.github.com>
2025-04-07 12:49:10 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250407/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,14 @@
+from sys import stdin as s
+
+# 정수가 차례로 주어지다가 0이 주어지면 0을 제외하고, 그때까지 주어진 정수를 차례로 출력
+# 그 수가 홀수이면 3을 더하고, 짝수이면 2로 나눈 몫 출력
+
+arr = list(map(int, s.readline().split()))
+
+for i in range(len(arr)):
+    if arr[i] == 0:
+        continue
+    elif arr[i] % 2 == 0:
+        print(arr[i] // 2, end = " ")
+    else:
+        print(arr[i] + 3, end = " ")
