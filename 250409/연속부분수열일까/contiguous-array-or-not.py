ecc2b2ff95bd831ce0df6cf609f7449858aa8a9f
seoli <139112248+sh694090@users.noreply.github.com>
2025-04-09 13:49:24 +0900

    Title: 연속부분수열일까, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250409/연속부분수열일까/contiguous-array-or-not.py
+++ 250409/연속부분수열일까/contiguous-array-or-not.py
@@ -10,17 +10,29 @@ B = list(map(int, s.readline().split()))
 # 만약 끝까지 다 있으면 Yes 출력
 # 아니면 No 출력
 
+# A_start는 A에서 B[0]가 있는 인덱스를 저장한 리스트
+A_start = []
 result = -1
-if B[0] in A:
-    A_start = A.index(B[0])
-    for i in range(len_B):
-        if B[i] == A[A_start + i]:
-            # print(B[i], A[A_start + i])
-            result = 1
-        else:
-            result = -1
-else:
-    result = -1
+for a in A:
+    if a == B[0]:
+        A_start.append(A.index(a))
+# print(A_start)
+
+# start_A의 모든 요소를 하나씩 순회
+# A 중 B[0]가 있는 인덱스 저장
+for i in range(len(A_start)):
+    # 처음에 A[start_A[0]]에 B[0]이 있지
+    if B[0] == A[A_start[i]]:
+        # A_start = A.index(B[0])
+        for j in range(len_B):
+            if B[j] == A[A_start[i] + j]:
+             # print(B[i], A[A_start + i])
+              result = 1
+            else:
+             # print(B[i], A[A_start + i])
+                result = -1
+    else:
+        result = -1
 
 if result == 1:
     print("Yes")
