5f3ad425dabd9e7f8fea013301c63775bf41707d
seoli <139112248+sh694090@users.noreply.github.com>
2025-04-11 14:03:48 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250411/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,23 @@
+from sys import stdin as s
+
+# 정수의 개수인 N 입력
+N = int(s.readline().rstrip())
+
+# N개의 정수 입력
+arr = list(map(int, s.readline().split()))
+
+# 주어진 정수들 중 최댓값의 위치 출력
+# 만약 최댓값이 여러 개인 경우 가장 왼쪽에 있는 최댓값의 위치 출력
+# 해당 최댓값의 위치보다 왼쪽에 있는 최댓값의 위치 출력
+# 이 과정을 계속 반복해서 마지막에 1이 나오면 종료
+
+# 최댓값 판별
+max_index = N - 1
+while max_index > 0:
+    for cur_val in arr:
+        max_val = 0
+        if max_val < cur_val:
+            max_val = cur_val
+    max_index = arr.index(max_val)
+    arr = arr[:max_index]
+    print(max_index + 1, end = ' ')
