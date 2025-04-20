c9c6451511478587b9f3ebd6be10f80bfdade693
seoli <139112248+sh694090@users.noreply.github.com>
2025-04-20 23:15:22 +0900

    Title: 정수 직사각형, Time: 69ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250420/정수 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,19 @@
+from sys import stdin as s
+
+n, m = map(int, s.readline().split())
+
+arr_2d = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+num = 1
+for i in range(n):
+    for j in range(m):
+        arr_2d[i][j] = num
+        num += 1
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end = " ")
+    print()
