07b9b655626f061167fa1f596eeaca1d90f9be1f
seoli <139112248+sh694090@users.noreply.github.com>
2025-05-06 15:10:04 +0900

    Title: 미는 횟수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250506/미는 횟수/number-of-pushes.py
@@ -0,0 +1,16 @@
+from sys import stdin as s
+
+A = s.readline().strip()
+B = s.readline().strip()
+
+cnt = 0
+for _ in range(len(A)):
+    if A == B:
+        print(cnt)
+        break
+    else:
+        cnt += 1
+        A = A[1:] + A[:1]
+
+if cnt == 6:
+    print(-1)
