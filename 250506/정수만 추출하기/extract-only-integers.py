57fbc5f29f7de27f1f612c8ea0f86a81ccad0d6a
seoli <139112248+sh694090@users.noreply.github.com>
2025-05-06 13:33:43 +0900

    Title: 정수만 추출하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250506/정수만 추출하기/extract-only-integers.py
+++ 250506/정수만 추출하기/extract-only-integers.py
@@ -3,23 +3,24 @@ from sys import stdin as s
 A, B = s.readline().split()
 
 arr_A = list(A)
-print(arr_A)
 num_A = []
 for i in range(len(arr_A)):
     if arr_A[i].isdigit() != True:
         break
     else:
-        num_A.append(arr_A)
+        num_A.append(arr_A[i])
 
-print(num_A)
+A = ''.join(num_A)
 
-# arr_B = list(B)
-# num_B = []
-# for i in range(len(arr_B)):
-#     if arr_B[i].isdigit() != True:
-#         break
-#     else:
-#         num_B.append(arr_B)
+arr_B = list(B)
+num_B = []
+for i in range(len(arr_B)):
+    if arr_B[i].isdigit() != True:
+        break
+    else:
+        num_B.append(arr_B[i])
 
-# result = int(''.join(arr_A)) + int(''.join(arr_B))
-# print(result)
+B = ''.join(num_B)
+
+result = int(A) + int(B)
+print(result)
